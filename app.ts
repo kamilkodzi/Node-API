0f166195e784e59b11b8d7105971e13339e069cb
kamilkodzi <kamil.adamski@ewywozy.pl>
2022-01-31 10:46:04 +0100

    Error handler for api created

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import logsRoutes from "./routes/logs";
 import ExpressError from "./helpers/ExpressError";
+import path from "path";
 
 const app = express();
 const port = 4050;
@@ -13,14 +14,20 @@ app.get("/", (req, res) => {
 });
 app.use("/api/logs", logsRoutes);
 
+app.get("/admin", (req, res) => {
+  throw new ExpressError("You are not autorized", 403);
+});
+
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = "Oh No, Something Went Wrong!";
-  res.status(statusCode).send("error", { err });
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.send(statusCode, {
+    status: statusCode,
+    message: message,
+  });
 });
 
 app.listen(port, () => {
