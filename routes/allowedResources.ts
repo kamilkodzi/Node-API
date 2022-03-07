4f87a5b9215e5994f2a5d16c1f246f49e7b7dbe4
Kamil Adamski <bizkamiladamski@gmail.com>
2022-03-07 12:37:31 +0100

    make a framework for allowedResources routes

--- routes/allowedResources.ts
+++ routes/allowedResources.ts
@@ -4,4 +4,44 @@ const router = Router();
 
 router.route("/allowedResources").get(allowedResourcesController.getRefreshAll);
 
+router.route("/allowedResources/:resourceName").get((req, res) => {
+  res.send(
+    "You choose to get all of the resources that stands behind: " +
+      req.params.resourceName
+  );
+});
+
+router.route("/allowedResources/:resourceName/:id").get((req, res) => {
+  res.send(
+    "You choose to see " +
+      req.params.resourceName +
+      "`s resource that has id equal to: " +
+      req.params.id
+  );
+});
+
+router.route("/allowedResources/:resourceName/new").post((req, res) => {
+  res.send(
+    "You choose to post/create a new instnce of: " + req.params.resourceName
+  );
+});
+
+router.route("/allowedResources/:resourceName/:id").put((req, res) => {
+  res.send(
+    "You choose to edit " +
+      req.params.resourceName +
+      "`s resource that has id equal to: " +
+      req.params.id
+  );
+});
+
+router.route("/allowedResources/:resourceName/:id").delete((req, res) => {
+  res.send(
+    "You choose to delete a " +
+      req.params.resourceName +
+      " that`s id is equal to: " +
+      req.params.id
+  );
+});
+
 export default router;
