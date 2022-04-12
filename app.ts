1360e2d0967759c6f16d659b2f1ca849105c5ed8
Kamil Adamski <bizkamiladamski@gmail.com>
2022-04-12 13:05:42 +0200

    security removed

--- app.ts
+++ app.ts
@@ -11,19 +11,21 @@ import basicAuth from "./middlewares/basicAuth";
 import lastUpdateRoutes from "./routes/lastUpdate";
 import allowedController from "./controllers/allowedResources";
 import makeUrlToLowerCase from "./mutators/toLowerCaseURL";
+import { allowCors } from "./middlewares/allowCorsDomain";
 
 const app = express();
 const port = 4050;
 app.use(express.json());
 app.use(helmet());
+app.use(allowCors);
 app.use(makeUrlToLowerCase);
 app.use(requestTime.requestTime);
 app.use(allowedController.refreshAllInBackgrourn);
 
-app.use(basicAuth, allowedResources);
-app.use(basicAuth, logsRoutes);
-app.use(basicAuth, errorsRoutes);
-app.use(basicAuth, lastUpdateRoutes);
+app.use(allowedResources);
+app.use(logsRoutes);
+app.use(errorsRoutes);
+app.use(lastUpdateRoutes);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Server could not understand your request", 400));
