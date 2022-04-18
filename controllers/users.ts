1a811e6fc026a8650e5fd21709c51e50707229e3
Kamil Adamski <bizkamiladamski@gmail.com>
2022-04-18 17:23:23 +0200

    console log ereased

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,6 +1,9 @@
 import userService from "../services/users";
 let counterLogin = 0;
+
 const authenticate = async ({ username, password }) => {
+  counterLogin++;
+  console.log("Sprawdzano login i hasło ", counterLogin, " razy");
   const userNameToLowerCase = username.toLowerCase();
   const user = await userService.authenticateViaBasicAuth(
     userNameToLowerCase,
